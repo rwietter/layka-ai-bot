2d872e4749107a636f84464db8d82de1ef6d844f
Maurício Witter <46854467+rwietter@users.noreply.github.com>
2023-02-17 23:26:47 -0300

    chore(.): add webhook deploy (#10)
    * chore(.): add webhook deploy

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,22 @@ import { setupRoutes } from './routes';
 import './database/db';
 
 setupRoutes();
-bot.launch();
+
+if(process.env.environment == 'PRODUCTION'){
+	bot.launch({
+		webhook:{
+			domain: 'https://layka.herokuapp.com/telegraf/3fc99407e2d79a3aa30901511a91c9b2b7bc7a60972ab7aa217dadf341b5',
+			port: 8000
+		}
+	}).then(() => {
+		console.info('production', bot.botInfo);
+	});
+} else {
+	bot.launch().then(() => {
+		console.info('local', bot.botInfo);
+	});
+}
+
+bot.telegram.getWebhookInfo().then((info) => {
+	console.log(info);
+});
