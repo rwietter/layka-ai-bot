f830977186e73b4fa9a90aa527f5eccd58d9dff8
Maurício Witter <mauriciobw17@gmail.com>
2023-02-17 23:36:10 -0300

    fix(.): add hash

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ setupRoutes();
 if(process.env.environment == 'PRODUCTION'){
 	bot.launch({
 		webhook:{
-			domain: 'https://layka.herokuapp.com/',
+			domain: 'https://layka.herokuapp.com/telegraf/3fc99407e2d79a3aa30901511a91c9b2b7bc7a60972ab7aa217dadf341b5',
 			port: 8000
 		}
 	}).then(() => {
