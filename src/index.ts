577661bb4e83063eb696ce8a23075f20cc5a6434
Maurício Witter <mauriciobw17@gmail.com>
2023-02-18 00:27:52 -0300

    chore(.): webhook

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,24 @@
 import { bot } from './http/http';
 import { setupRoutes } from './routes';
-import express from 'express';
-
-const app = express();
 
 import './database/db';
 
 setupRoutes();
 
-app.use(bot.webhookCallback('/bot'));
-
-if (process.env.NODE_ENV === 'production') {
-	bot.telegram.setWebhook('https://layka.herokuapp.com/bot');
+if(process.env.environment == 'PRODUCTION'){
+	bot.launch({
+		webhook:{
+			domain: 'layka.up.railway.app',
+			port: 8000
+		}
+	}).then(() => {
+		console.info('production', bot.botInfo);
+	});
 } else {
-	bot.launch();
+	bot.launch().then(() => {
+		console.info('local', bot.botInfo);
+	});
 }
-
-app.get('/', (req, res) => {
-	res.send('Our new tab!!');
-});
-
-app.listen(8000, '0.0.0.0', () => {
-	console.log(`Listen in the port ${8000}`);
-});
 
 bot.telegram.getWebhookInfo().then((info) => {
 	console.log(info);
